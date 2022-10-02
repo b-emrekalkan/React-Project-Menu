6d3574c3395c1954f694f5a2de847f6379dffeab
bemrekalkan <bemre.kalkan@gmail.com>
2022-10-02 20:24:36 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React-Project : Menu
+
+<p><img align="center" alt="gif" src="./src/menu.gif" width="500" height="320" /></p>
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header-coontainer">
+      <h1>Our Menu</h1>
+      <div className="underline"></div>
+    </div>
+  );
+};
+
+export default Header;
